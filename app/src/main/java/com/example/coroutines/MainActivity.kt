349fa268257f4bfc0eea28e1656c13899500d600
Okan Serdaroglu <okanserdaroglu2@gmail.com>
2022-07-28 11:28:54 +0300

    sequential background tasks with async and await

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -3,8 +3,8 @@ package com.example.coroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.async
@@ -16,6 +16,11 @@ import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * There are 2 jobs here and second one waits the first one's result.
+     * It is async and await pattern
+     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,30 +30,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    /**
-     * async and await pattern
-     * it is totally same with job pattern. There is only one difference. With this pattern
-     * you can access the result. In job pattern it is available it the job like that
-     * val job = launch {
-     *    val result = getResult1FromAPI() -> result is available only in job. But in async pattern you can get it outside the job
-     * }
-     */
+
     private fun fakeApiRequest() {
-
         CoroutineScope(IO).launch {
-
             val executionTime = measureTimeMillis {
-                val result1: Deferred<String> = async {
-                    println("debug: launching job1:${Thread.currentThread().name}")
+                val result1 = async {
+                    println("debug: launching job1: ${Thread.currentThread().name}")
                     getResult1FromAPI()
-                }
+                }.await()
 
-                val result2: Deferred<String> = async {
-                    println("debug: launching job2:${Thread.currentThread().name}")
-                    getResult2FromAPI()
-                }
+                println("Debug: got result2: $result1")
+                setTextOnMainThread(result1)
+
+                val result2 = async {
+                    println("debug: launching job2: ${Thread.currentThread().name}")
+                    getResult2FromAPI(result1)
+                }.await()
+
+                println("Debug: got result2: $result2")
+                setTextOnMainThread(result2)
             }
-            println("debug:total time elapsed:${executionTime}")
+
+            println("Debug: total elapsed time: $executionTime ms")
         }
     }
 
@@ -68,9 +71,12 @@ class MainActivity : AppCompatActivity() {
         return "RESULT #1"
     }
 
-    private suspend fun getResult2FromAPI(): String {
+    private suspend fun getResult2FromAPI(result1: String): String {
         delay(1700)
-        return "RESULT #2"
+        if (result1 == "RESULT #1") {
+            return "RESULT #2"
+        }
+        throw CancellationException("Result1 was incorrect...")
     }
 
 }
